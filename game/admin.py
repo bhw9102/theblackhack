e460aeb18e356ec58f4eca14de3d79c037c7c3d1
hyunwoo <bhw9102@gmail.com>
2018-11-30 16:26:26 +0900

    모델을 추가하고 어드민에서 관리할 수 있게하다.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from game.models import *
+
+
+class JobClassAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', )
+    list_display_links = ('id', 'name', )
+
+
+class EquipmentClassAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'name', 'price', 'defence')
+    list_display_links = ('id', 'name', )
+    list_filter = ('price', 'defence', )
+
+
+admin.site.register(JobClass, JobClassAdmin)
+admin.site.register(EquipmentClass, EquipmentClassAdmin)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class JobClass(models.Model):
+    name = models.CharField(max_length=32, verbose_name="이름")
+
+
+class EquipmentClass(models.Model):
+    name = models.CharField(max_length=32, verbose_name="이름")
+    price = models.PositiveSmallIntegerField(verbose_name="가격")
+    defence = models.PositiveSmallIntegerField(verbose_name="방어점")
+
